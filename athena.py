85d6e958da917310ee5f68eed28a0000d944b400
doctorthe113 <code.doc113@gmail.com>
2024-02-24 11:59:52 +0600

    added an alias of i am bored

--- athena.py
+++ athena.py
@@ -233,14 +233,7 @@ async def tell(ctx, *, arg):
     await ctx.send(phraseObj.dad_joke_method())
 
 # for bored. eg: "i am bored"
-@bot.command()
-async def i(ctx, *, arg):
-    if not arg.startswith("am bored"):
-        return None
-    await ctx.send(f"Here's an activity for you: {phraseObj.bored()}")
-
-# for bored but the capital letter version. eg: "I am bored"
-@bot.command()
+@bot.command(aliases = ["I"])
 async def i(ctx, *, arg):
     if not arg.startswith("am bored"):
         return None
