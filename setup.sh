ae6d0ed16a584c59923e619f83281495919375dc
doctorthe113 <code.doc113@gmail.com>
2024-06-11 20:59:50 +0600

    upodated gitignore

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 git checkout master
-git pull
+git fetch --all
+git reset --hard origin/master
 python3 -m venv .venv
 source .venv/bin/activate
 .venv/bin/pip install --no-cache-dir --upgrade -r requirements.txt
