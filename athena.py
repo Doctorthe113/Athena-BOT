0bec386d288672b95ce3a6a70518e36c5ddbcd8a
doctorthe113 <code.doc113@gmail.com>
2025-02-23 08:09:00 +0600

    updated yt-dlp

--- athena.py
+++ athena.py
@@ -757,7 +757,7 @@ async def before_ping_doc():
 
 if __name__ == "__main__":
     change_status.start()
-    ping_doc.start()
+    # ping_doc.start()
     desco_balance_checker.start()
     # birthday_reminder.start()
     bot.run(TOKEN)
